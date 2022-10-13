8e996cd2e1da0d4132ee0f009c1760c9c0e7f0c4
Mahmudul Alam <mahmudula2000@gmail.com>
2022-10-13 06:44:37 +0600

    added signal to automatically record changes

--- todo/apps.py
+++ todo/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class TodoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'todo'
+
+    def ready(self):
+        from . import signals
+        super().ready()
